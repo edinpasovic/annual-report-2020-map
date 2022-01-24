28bc11d710c1edb30928708c11340b7cb26ccd84
Edin Pasovic <edin@occrp.org>
2022-01-24 21:18:41 +0100

    all centres added

--- script/script.js
+++ script/script.js
@@ -1,11 +1,11 @@
 const map = L.map('map', {
-    center: [48.208, 20],
-    zoom: 5
+    center: [30, 26],
+    zoom: 3
 });
 
 L.tileLayer('https://api.mapbox.com/styles/v1/villewilson/ckylnlbyv030614tf77qxuy1s/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> OCCRP, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 7,
+    maxZoom: 8,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
@@ -275,5 +275,249 @@ const member27 = L.marker([50.45, 30.53]).addTo(map).bindPopup(`
         <div class="popup-list">
             <h3>Media Development Foundation</h3><hr>
             <span>Kyiv, Ukraine</span>
+    </div>
+`);
+
+const member28 = L.marker([55.75, 37.61]).addTo(map).bindPopup(`
+    <div class="popup">
+        <img src="./images/Istories.png">
+        <div class="separator"></div>
+        <div class="popup-list">
+            <h3>iStories</h3><hr>
+            <span>Moscow, Russia</span>
+    </div>
+`);
+
+const member29 = L.marker([55.75, 37.71]).addTo(map).bindPopup(`
+    <div class="popup">
+        <div class="popup-list">
+            <h3>Novaya Gazeta</h3><hr>
+            <span>Moscow, Russia</span>
+    </div>
+`);
+
+const member30 = L.marker([52.23, 21.04]).addTo(map).bindPopup(`
+    <div class="popup">
+        <img src="./images/Istories.png">
+        <div class="separator"></div>
+        <div class="popup-list">
+            <h3>Fundacja Reporterów - the Reporters Foundation</h3><hr>
+            <span>Poland, Warsaw</span>
+    </div>
+`);
+
+const member31 = L.marker([48.14, 17.1]).addTo(map).bindPopup(`
+    <div class="popup">
+        <div class="popup-list">
+            <h3>Investigative Center of Jan Kuciak</h3><hr>
+            <span>Bratislava, Slovak Republic</span>
+    </div>
+`);
+
+const member32 = L.marker([46.05, 14.5]).addTo(map).bindPopup(`
+    <div class="popup">
+        <img src="./images/Ostro.png">
+        <div class="separator"></div>
+        <div class="popup-list">
+            <h3>Ostro</h3><hr>
+            <span>Ljubljana, Slovenia</span>
+    </div>
+`);
+
+const member33 = L.marker([36.8, 10.18]).addTo(map).bindPopup(`
+    <div class="popup">
+        <div class="popup-list">
+            <h3>Inkyfada</h3><hr>
+            <span>Tunis, Tunisia</span>
+    </div>
+`);
+
+const member34 = L.marker([31.94, 35.91]).addTo(map).bindPopup(`
+    <div class="popup">
+        <img src="./images/Arij.png">
+        <div class="separator"></div>
+        <div class="popup-list">
+            <h3>ARIJ - Arab Reporters for Investigative Journalism</h3><hr>
+            <span>Amman, Jordan</span>
+    </div>
+`);
+
+const member35 = L.marker([42.97, 74.6]).addTo(map).bindPopup(`
+    <div class="popup">
+        <img src="./images/Kloop.png">
+        <div class="separator"></div>
+        <div class="popup-list">
+            <h3>Kloop Media</h3><hr>
+            <span>Bishkek, Kyrgyzstan</span>
+    </div>
+`);
+
+const member36 = L.marker([42.97, 74.7]).addTo(map).bindPopup(`
+    <div class="popup">
+        <div class="popup-list">
+            <h3>Radio Free Europe / Radio Liberty</h3><hr>
+            <span>Bishkek, Kyrgyzstan</span>
+    </div>
+`);
+
+const member37 = L.marker([6.13, 1.22]).addTo(map).bindPopup(`
+    <div class="popup">
+        <div class="popup-list">
+            <h3>L'Alternative</h3><hr>
+            <span>Lome, Togo</span>
+    </div>
+`);
+
+const member38 = L.marker([13.51, 2.11]).addTo(map).bindPopup(`
+    <div class="popup">
+        <div class="popup-list">
+            <h3>L'Evenement</h3><hr>
+            <span>Niamey, Niger</span>
+    </div>
+`);
+
+const member39 = L.marker([-24.63, 25.91]).addTo(map).bindPopup(`
+    <div class="popup">
+        <div class="popup-list">
+            <h3>Center for Investigative Journalism - INK</h3><hr>
+            <span>Gaborone, Botswana</span>
+    </div>
+`);
+
+const member40 = L.marker([-11.44, 34]).addTo(map).bindPopup(`
+    <div class="popup">
+        <div class="popup-list">
+            <h3>MCIJ - Malawi Center for Investigative Journalism</h3><hr>
+            <span>Mzuzu, Malawi</span>
+    </div>
+`);
+
+const member41 = L.marker([6.29, -10.78]).addTo(map).bindPopup(`
+    <div class="popup">
+        <div class="popup-list">
+            <h3>New Narratives</h3><hr>
+            <span>Monrovia, Liberia</span>
+    </div>
+`);
+
+const member42 = L.marker([-22.56, 17.08]).addTo(map).bindPopup(`
+    <div class="popup">
+        <div class="popup-list">
+            <h3>Namibian</h3><hr>
+            <span>Windhoek, Namibia</span>
+    </div>
+`);
+
+const member43 = L.marker([-25.48, 30.97]).addTo(map).bindPopup(`
+    <div class="popup">
+        <div class="popup-list">
+            <h3>Oxpeckers</h3><hr>
+            <span>Mbombela, South Africa</span>
+    </div>
+`);
+
+const member44 = L.marker([12.64, -8]).addTo(map).bindPopup(`
+    <div class="popup">
+        <div class="popup-list">
+            <h3>Depeche du Mali</h3><hr>
+            <span>Bamako, Republic of Mali</span>
+    </div>
+`);
+
+const member45 = L.marker([9.05, 7.41]).addTo(map).bindPopup(`
+    <div class="popup">
+        <div class="popup-list">
+            <h3>Premium Times</h3><hr>
+            <span>Abuja, Nigeria</span>
+    </div>
+`);
+
+const member46 = L.marker([-1.29, 36.83]).addTo(map).bindPopup(`
+    <div class="popup">
+        <div class="popup-list">
+            <h3>Africa Uncensored</h3><hr>
+            <span>Nairobi, Kenya</span>
+    </div>
+`);
+
+const member47 = L.marker([-1.29, 36.93]).addTo(map).bindPopup(`
+    <div class="popup">
+        <img src="./images/Elephant.png">
+        <div class="separator"></div>
+        <div class="popup-list">
+            <h3>The Elephant</h3><hr>
+            <span>Nairobi, Kenya</span>
+    </div>
+`);
+
+const member48 = L.marker([-20.16, 57.49]).addTo(map).bindPopup(`
+    <div class="popup">
+        <div class="popup-list">
+            <h3>Le Defi Media Group</h3><hr>
+            <span>Port-Louis, Mauritius</span>
+    </div>
+`);
+
+const member49 = L.marker([-25.97, 32.58]).addTo(map).bindPopup(`
+    <div class="popup">
+        <div class="popup-list">
+            <h3>Verdade</h3><hr>
+            <span>Maputo, Mozambique</span>
+    </div>
+`);
+
+const member50 = L.marker([-33.92, 18.42]).addTo(map).bindPopup(`
+    <div class="popup">
+        <div class="popup-list">
+            <h3>Noseweek</h3><hr>
+            <span>Cape Town, South Africa</span>
+    </div>
+`);
+
+const member51 = L.marker([51.5, -0.12]).addTo(map).bindPopup(`
+    <div class="popup">
+        <img src="./images/AfricaConfidential.png">
+        <div class="separator"></div>
+        <div class="popup-list">
+            <h3>Africa Confidential</h3><hr>
+            <span>Undisclosed location in UK</span>
+    </div>
+`);
+
+const member52 = L.marker([5.56, -0.18]).addTo(map).bindPopup(`
+    <div class="popup">
+        <img src="./images/NewsBridge.jpg">
+        <div class="separator"></div>
+        <div class="popup-list">
+            <h3>News Bridge Africa</h3><hr>
+            <span>Accra, Ghana</span>
+    </div>
+`);
+
+const member53 = L.marker([-15.41, 28.3]).addTo(map).bindPopup(`
+    <div class="popup">
+        <img src="./images/Diggers.png">
+        <div class="separator"></div>
+        <div class="popup-list">
+            <h3>Newsdiggers Media Limited</h3><hr>
+            <span>Lusaka, Zambia</span>
+    </div>
+`);
+
+const member54 = L.marker([-26.2, 28.03]).addTo(map).bindPopup(`
+    <div class="popup">
+        <img src="./images/MailGuardian.png">
+        <div class="separator"></div>
+        <div class="popup-list">
+            <h3>Mail & Guardian</h3><hr>
+            <span>Johannesburg, South Africa</span>
+    </div>
+`);
+
+const member55 = L.marker([9.93, -84.1]).addTo(map).bindPopup(`
+    <div class="popup">
+        <div class="popup-list">
+            <h3>Centro Latino Americano de Investigacion Periodistica (CLIP)</h3><hr>
+            <span>San Jose, Costa Rica</span>
     </div>
 `);
